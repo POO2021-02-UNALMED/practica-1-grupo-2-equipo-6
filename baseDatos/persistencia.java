d554811771103bdc9da8e9197150f53a9b12c089
Mauricio Mesa <jommesaec@unal.edu.co>
2021-12-16 13:18:27 -0500

    Update persistencia.java

--- baseDatos/persistencia.java
+++ baseDatos/persistencia.java
@@ -6,6 +6,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import gestorAplicacion.Loadout.Inventario;
+import gestorAplicacion.pjs.Player;
 
 public class persistencia {
     static String fichero = System.getProperty("user.dir");
@@ -14,12 +15,18 @@ public class persistencia {
         try{
             
             ObjectOutputStream jugador = new ObjectOutputStream(new FileOutputStream(fichero+"\\baseDatos\\temp\\pj.dat"));
+            System.out.println(gestorAplicacion.pjs.Player.clase.name());
+            //gestorAplicacion.pjs.Player.getClase().setNombre(gestorAplicacion.pjs.Player.clase.name());            
             jugador.writeObject(gestorAplicacion.pjs.Player.player);
             jugador.close();            
             
             ObjectOutputStream escenaActualSaved = new ObjectOutputStream(new FileOutputStream(fichero+"\\baseDatos\\temp\\scene.dat"));
             escenaActualSaved.writeObject(gestorAplicacion.mecanicas.Narrador.escenaActual);
             escenaActualSaved.close();
+
+            ObjectOutputStream claseSaved = new ObjectOutputStream(new FileOutputStream(fichero+"\\baseDatos\\temp\\class.dat"));
+            claseSaved.writeObject(gestorAplicacion.pjs.Player.getClase());
+            claseSaved.close();
 
         }catch(Exception e){
             e.printStackTrace();
@@ -29,10 +36,20 @@ public class persistencia {
     public static void leerFichero(){
         try{
             ObjectInputStream dataPj = new ObjectInputStream(new FileInputStream(fichero+"\\baseDatos\\temp\\pj.dat"));
+            
+            
             gestorAplicacion.pjs.Player playerData = (gestorAplicacion.pjs.Player) dataPj.readObject();            
+            gestorAplicacion.pjs.Player.player = playerData;
+            
 
             ObjectInputStream dataEsc = new ObjectInputStream(new FileInputStream(fichero+"\\baseDatos\\temp\\scene.dat"));
             int escenaActualSavedData = (int) dataEsc.readObject();
+
+            ObjectInputStream dataClase = new ObjectInputStream(new FileInputStream(fichero+"\\baseDatos\\temp\\class.dat"));
+            gestorAplicacion.pjs.Clase playerClass = (gestorAplicacion.pjs.Clase) dataClase.readObject();
+
+            Player.clase = playerClass;
+            //
 
             gestorAplicacion.mecanicas.Narrador.escenaActual = escenaActualSavedData;
             
